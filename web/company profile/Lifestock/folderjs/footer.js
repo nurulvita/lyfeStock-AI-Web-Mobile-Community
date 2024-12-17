db89b164e97c8e1cf8ffa270ecc0914e48d79dc6
erik rahman <118335576+erikrahman2@users.noreply.github.com>
2024-12-17 10:41:07 +0700

    Delete web/company profile/Lifestock directory

--- web/company profile/Lifestock/folderjs/footer.js
+++
@@ -1,9 +0,0 @@
-// loadfoot
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("footer.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("footer").innerHTML = data;
-    })
-    .catch((error) => console.error("Error loading footer:", error));
-});
